aed93c535163cd07e62b690a95faff5e25c956eb
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-12-01 16:21:31 -0700

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -41,35 +41,12 @@ self.addEventListener("activate", function (event) {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log('IT HAS HAPPENED', event);
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(DATA_CACHE).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(error => {
-            return cache.match(event.request);
-          });
-      }).catch(error => console.log(error))
-    );
-    return;
-  }
-
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/");
-        }
-      });
-    })
+    fetch(event.request)
+      .catch(function () {
+        return caches.match(event.request);
+      })
   );
 });
