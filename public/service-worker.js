fab3daf0323c31de62409df8de275fa4570c3277
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-12-01 10:33:22 -0700

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(caches.open(CACHE).then(function(cache) {
       return cache.addAll([
         '/',
+        '/index.html',
         './styles.css',
+        'db.js',
         '/index.js',
         '/icons/icon-192x192.png',
         '/icons/icon-512x512.png',
