5b6299d6768b9fcf29c5c7a63923e944fefa1d74
Sydney Goodwill <sydneyjogoodwill@gmail.com>
2020-12-01 15:58:14 -0700

    changed location of manifest file, modified html and service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 console.log("service worker working!");
 
 const CACHE = "best-cache-ever";
+const DATA_CACHE = "data-cache";
 
 self.addEventListener('install', function(event) {
   event.waitUntil(caches.open(CACHE).then(function(cache) {
@@ -21,27 +22,54 @@ self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(event) {
-  console.log("Helloooo");
+self.addEventListener("activate", function (event) {
+  console.log("activation working");
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE) {
-            console.log("YOU HAVE BEEN DELETED", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    }))
-})
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE) {
+              console.log("YOU HAVE BEEN DELETED", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+      .catch(error => console.log("activation error: ", error))
+  );
+  self.clients.claim();
+});
 
 self.addEventListener('fetch', function(event) {
   console.log('IT HAS HAPPENED', event);
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(error => {
+            return cache.match(event.request);
+          });
+      }).catch(error => console.log(error))
+    );
+    return;
+  }
 
   event.respondWith(
-    caches.match(event.request).then(function(resp) {
-      return resp || fetch(event.request);
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
     })
   );
 });
